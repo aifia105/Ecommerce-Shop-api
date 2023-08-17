1199ab2948cc7dc103acb043380e75655344bb68
Aifia Mohamed <mohamedaifia34@gmail.com>
2023-08-17 03:54:02 +0100

    swagger ui implementation

---
+++ src/main/java/com/PersonalProject/Jemo/config/OpenApiConfig.java
@@ -0,0 +1,43 @@
+package com.PersonalProject.Jemo.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Aifia",
+                        email = "mohamedaifia34@gmail.com"
+                ),
+                description = "Jemo Swagger UI documentation",
+                title = "Swagger UI specification -  Jemo"
+        ),
+        servers = {
+                @Server(
+                        description = "local ENV",
+                        url = "http://localhost:8080"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
--- src/main/java/com/PersonalProject/Jemo/controller/AuthenticationCustomerController.java
+++ src/main/java/com/PersonalProject/Jemo/controller/AuthenticationCustomerController.java
@@ -8,6 +8,9 @@ import com.PersonalProject.Jemo.dto.auth.AuthenticationRequest;
 import com.PersonalProject.Jemo.dto.auth.AuthenticationResponse;
 import com.PersonalProject.Jemo.model.Customer;
 import com.PersonalProject.Jemo.services.Implemenation.AuthenticationCustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,15 +21,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = AUTHENTICATION_ENDPOINT)
 @RequiredArgsConstructor
+@Tag(name = "Authentication" )
 public class AuthenticationCustomerController {
 
     private final AuthenticationCustomerService customerService;
 
+    @Operation(
+            description = "Register new customer",
+            summary = "create new customer account",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Bad request",
+                            responseCode = "400"
+                    )
+            }
+    )
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody Customer register){
         return ResponseEntity.ok(customerService.register(register));
     }
 
+    @Operation(
+            description = "Authenticate",
+            summary = "authenticate to your account",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Unauthorized / Invalid token",
+                            responseCode = "403"
+                    )
+            }
+    )
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest){
         return ResponseEntity.ok(customerService.authenticate(authenticationRequest));
