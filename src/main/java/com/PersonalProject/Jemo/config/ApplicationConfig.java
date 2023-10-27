07f6ee6e0c0d8e029398bba426fdc73c46ebe0f2
Aifia Mohamed <mohamedaifia34@gmail.com>
2023-10-27 05:08:04 +0100

    auth-update-final

--- src/main/java/com/PersonalProject/Jemo/config/ApplicationConfig.java
+++ src/main/java/com/PersonalProject/Jemo/config/ApplicationConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
@@ -31,6 +33,19 @@ public class ApplicationConfig {
                 .orElseThrow(()-> new EntityNotFoundException("No client found with this email",ErrorCodes.USER_NOT_FOUND));
 
 
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
     }
 
     @Bean
--- src/main/java/com/PersonalProject/Jemo/config/SecurityConfiguration.java
+++ src/main/java/com/PersonalProject/Jemo/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,8 +26,6 @@ public class SecurityConfiguration  {
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
             httpSecurity
-                    .csrf()
-                    .disable()
                     .authorizeHttpRequests((request) -> request
                             .requestMatchers("/JemoApi/version1/singin",
                                     "/JemoApi/version1/login",
@@ -39,6 +38,8 @@ public class SecurityConfiguration  {
                                     "/swagger-ui.html"
                             ).permitAll()
                             .anyRequest().authenticated())
+                    .csrf(csrf -> csrf.disable())
+                    .cors(Customizer.withDefaults())
                     .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                     .authenticationProvider(authenticationProvider)
                     .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
